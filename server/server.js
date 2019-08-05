03d0df97c594ee1adc892c8a8b97f054cf4a8f15
ThanMatt <belphegorous@gmail.com>
2019-08-05 19:33:59 +0800

    Added a few dependencies

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
+const helmet = require('helmet');
 
 require('dotenv').config();
 
@@ -10,6 +12,8 @@ const port = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(morgan('combined'));
+app.use(helmet());
 
 const uri = process.env.ATLAS_URI; //!! Create a .env file for this
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
