7ae70a9c0455103662430d5f3a7d4c1bbfe9b662
ThanMatt <belphegorous@gmail.com>
2019-08-05 09:26:35 +0800

    Updated dependencies

--- server/server.js
+++ server/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
+const port = process.env.PORT || 4000;
 
-app.listen(4000, () => {
-  console.log('Listening to port 4000!')
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Listening to port ${port}!`)
 })
